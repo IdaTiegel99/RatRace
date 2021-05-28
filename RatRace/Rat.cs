f58cf0708f280126781c6dca47e2df3cd4541d56
JWittstrom <jonaswittstrom1@gmail.com>
2021-05-28 10:58:29 +0200

    public

--- RatRace/Rat.cs
+++ RatRace/Rat.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RatRace
 {
-    class Rat
+    public class Rat
     {
     }
 }
--- RatRace/User.cs
+++ RatRace/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RatRace
 {
-    class User
+    public class User
     {
     }
 }
