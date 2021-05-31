59b4d9f398d90dab9422bfc2e73ae578372660cf
Kr1qa <christofferlinde@live.dk>
2021-05-31 08:24:32 +0200

    Update

--- RatRace/Bet.cs
+++ RatRace/Bet.cs
@@ -12,7 +12,8 @@ namespace RatRace
     {
         public Player PlayerID;
         public Rat RatID;
-        public int Amount;   
+        public int Amount;
+        public int BetID;
     }
         
 
