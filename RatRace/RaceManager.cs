64738effaf1a6645761a740c884f45ae97e09c18
JWittstrom <jonaswittstrom1@gmail.com>
2021-05-31 08:30:21 +0200

    uses

--- RatRace/RaceManager.cs
+++ RatRace/RaceManager.cs
@@ -1,4 +1,10 @@
-﻿namespace RatRace
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatRace
 {
     public class RaceManager 
     {
