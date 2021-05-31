f10fe02580d7cbce040b130593e5b5ec07704714
Kr1qa <christofferlinde@live.dk>
2021-05-31 08:23:47 +0200

    Update

--- RatRace/Bet.cs
+++ RatRace/Bet.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Hvis man spiller selv er satsen 1/0.5. Så hvis man better 100kr får man 150kr igen.
+
 namespace RatRace
 {
     public class Bet
     {
-
+        public Player PlayerID;
+        public Rat RatID;
+        public int Amount;   
     }
+        
+
 }
