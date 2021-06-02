8bc59d3f731ec3f1b27d42c702005e516234f368
Kr1qa <christofferlinde@live.dk>
2021-06-02 10:39:41 +0200

    FinishLine WIP

--- RatRace/Main.Designer.cs
+++ RatRace/Main.Designer.cs
@@ -173,7 +173,7 @@ namespace RatRace
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 644);
+            this.ClientSize = new System.Drawing.Size(834, 641);
             this.Controls.Add(this.pictureRat4);
             this.Controls.Add(this.pictureRat3);
             this.Controls.Add(this.pictureRat5);
--- RatRace/Main.cs
+++ RatRace/Main.cs
@@ -13,24 +13,23 @@ namespace RatRace
     public partial class Main : Form
     {
         RaceManager rm;
-        Rat ratID;
         PictureBox[] images = new PictureBox[5];
         Random rnd = new Random();
         public Main()
         {
             InitializeComponent();
-            rm  = new RaceManager();
+            rm = new RaceManager();
             images[0] = pictureRat1;
+            images[1] = pictureRat2;
+            images[2] = pictureRat3;
+            images[3] = pictureRat4;
+            images[4] = pictureRat5;
+
         }
-        public Main (Rat rats)
-        {
-            InitializeComponent();
-            ratID = rats;
-        }
-      
+
         private void BtnPlayer1_Click(object sender, EventArgs e)
         {
-            BetScreen Bscr = new BetScreen(rm.Players[0],rm);
+            BetScreen Bscr = new BetScreen(rm.Players[0], rm);
             Bscr.ShowDialog();
         }
 
@@ -60,11 +59,26 @@ namespace RatRace
 
         private void BtnMove_Click(object sender, EventArgs e)
         {
-            images[0].Location = new Point(images[0].Location.X +rnd.Next(1,21) +rnd.Next(1,21) , images[0].Location.Y);
+            images[0].Location = new Point(images[0].Location.X + rnd.Next(1, 21) + rnd.Next(1, 21), images[0].Location.Y);
+            //images[1].Location = new Point(images[1].Location.X + rnd.Next(1, 21) + rnd.Next(1, 21), images[1].Location.Y);
+            //images[2].Location = new Point(images[2].Location.X + rnd.Next(1, 21) + rnd.Next(1, 21), images[2].Location.Y);
+            //images[3].Location = new Point(images[3].Location.X + rnd.Next(1, 21) + rnd.Next(1, 21), images[3].Location.Y);
+            //images[4].Location = new Point(images[4].Location.X + rnd.Next(1, 21) + rnd.Next(1, 21), images[4].Location.Y);
+
         }
 
-       
+        public void FinishLine()
+        {
+            InitializeComponent();
+            string msg = "Hej du vandt!";
 
-       
+             if (images[0].Location.X == 710 )
+            {
+                MessageBox.Show(msg);
+            }
+        }
+
+
+
     }
 }
